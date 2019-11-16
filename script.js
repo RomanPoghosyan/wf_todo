94e8004b4e80797aef7ed7cbfee0496ff2e00a43
RomanPoghosyan <roman.poghos@gmail.com>
2019-11-16 11:27:32 +0400

    done

--- script.js
+++ script.js
@@ -3,21 +3,31 @@ const container = document.getElementById("tasks");
 const button = document.getElementById("button");
 
 function addTask(){
-    let li = document.createElement("li");
+    let div = document.createElement("div");
+    div.className = "task";
     let value = input.value;
     input.value = "";
-    li.innerHTML = `<span class="body">${value}</span><span class="remove">X</span>`
-    container.append(li);
+    div.innerHTML = `<div class="alert alert-warning alert-dismissible fade show pr-3" role="alert">
+                      <span class="body">${value}</span>
+                      <span class="remove">&times;</span>
+                    </div>`
+    container.append(div);
 }
 
 function clickHendler(event){
-    if(event.target.className == "body"){
-
+    if(event.target.classList.contains("body")){
+        event.target.classList.toggle("lineThrough");
     }else if(event.target.className == "remove"){
-
+        event.target.closest(".task").remove();
     }
-    console.log(event.target.className);
+}
+
+function enterHendler(event){
+    if(event.which == "13"){
+        addTask();
+    }
 }
 
 container.addEventListener("click", clickHendler);
 button.addEventListener("click", addTask);
+input.addEventListener("keydown", enterHendler)
