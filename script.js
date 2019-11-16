5f91a7f346541c757cfca446d50f5237034eb8a7
RomanPoghosyan <roman.poghos@gmail.com>
2019-11-16 10:19:26 +0400

    fixes

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("add");
+const container = document.getElementById("tasks");
+const button = document.getElementById("button");
+
+function addTask(){
+    let li = document.createElement("li");
+    let value = input.value;
+    input.value = "";
+    li.innerHTML = `<span class="body">${value}</span><span class="remove">X</span>`
+    container.append(li);
+}
+
+function clickHendler(event){
+    if(event.target.className == "body"){
+
+    }elseif(event.target.className == "remove"){
+
+    }
+    console.log(event.target.className);
+}
+
+container.addEventListener("click", clickHendler);
+button.addEventListener("click", addTask);
